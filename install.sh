a4f3b074f1b5fde8baf3e064cd0be185e0a61a68
nohashduck <98825772+nohashduck@users.noreply.github.com>
2023-07-08 15:29:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ yes | cp -r "$(pwd)/home/nohashduck/.icons" "/home/$name/"
 yes | cp -r "$(pwd)/home/nohashduck/.themes" "/home/$name/"
 yes | cp -r "$(pwd)/home/nohashduck/Pictures" "/home/$name/"
 yes | cp -r "$(pwd)/home/nohashduck/.bash_profile" "/home/$name/"
-yes | cp -r "$(pwd)/home/nohashduck/.vimrc" "/home/$name/"
 yes | cp -r "$(pwd)/etc" "/"
 yes | chmod -R 777 "/home/$name"
 sudo pacman -S neofetch kitty neovim ranger firefox gnome-boxes sway swaybg waybar wofi mako grim noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome alsa-lib alsa-utils pulseaudio file-roller nautilus htop wl-clipboard zenity telegram-desktop
